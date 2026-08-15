b52e0d177947b021c9c5b7192dc3f7634c08bbee
bhavya18saisree <bhavyasaisree2004@gmail.com>
2026-08-15 23:36:45 +0530

    Fix Render allowed hosts

--- careergraph/settings.py
+++ careergraph/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-wo@a+1b#yxc*$mw+nm18av6+fp#g(f8dp2ff+=c&s@@qwj(#t@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ALLOWED_HOSTS = [
+    "careergraphai.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]]
 
 
 # Application definition
