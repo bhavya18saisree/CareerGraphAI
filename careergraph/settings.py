5be0279e9bb6d239c5f680312b7cb32ab6e39f09
bhavya18saisree <bhavyasaisree2004@gmail.com>
2026-08-16 10:06:55 +0530

    Fix project static files

--- careergraph/settings.py
+++ careergraph/settings.py
@@ -125,8 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
